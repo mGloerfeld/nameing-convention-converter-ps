6d782520310c40e4d1992b1692a2764cd6bfe619
Matthias Gloerfeld <matthias_gloerfeld@extern.deichmann.com>
2025-10-29 07:47:22 +0100

    update ToCamelSnakeCase

--- src/lib/utils/to-camel-snake-case.ps1
+++ src/lib/utils/to-camel-snake-case.ps1
@@ -1,23 +1,57 @@
 
-function ToCamelSnakeCase() {
-    
- param
-   (
-      [Parameter(Mandatory=$true, Position=0, ValueFromPipeline = $true)]
-      [ValidateNotNull()]
-      [String[]] $value 
-   )
+<#
+.SYNOPSIS
+    Converts an array of words to camel snake case format.
+
+.DESCRIPTION
+    Transforms an array of words into camel snake case notation where the first word 
+    is lowercase and subsequent words are capitalized, all separated by underscores.
+    Example: "hello", "world", "test" becomes "hello_World_Test"
+
+.PARAMETER value
+    An array of strings representing individual words to be converted.
+
+.INPUTS
+    String[] - An array of words to convert.
+
+.OUTPUTS
+    String - The camel snake case formatted string.
+
+.EXAMPLE
+    @("hello", "world") | ToCamelSnakeCase
+    Returns: "hello_World"
+
+.NOTES
+    This function is designed to work with the pipeline and processes each word
+    to build the final camel snake case string.
+#>
+function ToCamelSnakeCase {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory = $true, Position = 0, ValueFromPipeline = $true)]
+        [ValidateNotNull()]
+        [String[]] $value 
+    )
  
- BEGIN {
-  $str = "";
- }
+    BEGIN {
+        $result = ""
+        $isFirstWord = $true
+    }
 
- PROCESS {
-      
-          $str += $($value.ToLower() + "_");
- }
+    PROCESS {
+        if ($isFirstWord) {
+            # First word should be lowercase
+            $result += $value.ToLower()
+            $isFirstWord = $false
+        }
+        else {
+            # Subsequent words should be capitalized and preceded by underscore
+            $capitalizedWord = $value.Substring(0, 1).ToUpper() + $value.Substring(1).ToLower()
+            $result += "_" + $capitalizedWord
+        }
+    }
 
-  END {
-   return $str.Substring(0, $str.Length - 1)
-   }
+    END {
+        return $result
+    }
 }
